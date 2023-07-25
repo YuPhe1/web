c4a146f6fd06f20805cebfdb583d4d574bf732ee
YuPhe1 <oksunboy1@naver.com>
2023-07-25 13:25:07 +0900

    23.07.25

--- ex02/src/main/java/controller/ProductController.java
+++ ex02/src/main/java/controller/ProductController.java
@@ -30,7 +30,7 @@ public class ProductController extends HttpServlet {
 		}
 		RequestDispatcher dis = request.getRequestDispatcher("/home.jsp");
 		dis.forward(request, response);
-		 
+		
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
